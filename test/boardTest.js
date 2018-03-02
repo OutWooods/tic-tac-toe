2c1ff30653be3651c1a3419b52ca17dffe4a16d2
Tagrand <thomas.a.grand@gmail.com>
2018-03-02 15:33:38 +0000

    board contains only null at stary

--- test/boardTest.js
+++ test/boardTest.js
@@ -9,16 +9,28 @@ describe('Board', function() {
     it('should return an array with a length of 9 after initialising', function() {
       console.log(Board)
       let board = new Board();
-      expect(board.current().length).equal(9)
+      assert.lengthOf(board.current(), (9))
     });
 
-    it('should add an X to the board', () => {
+    it('should contain only null', function() {
+      let board = new Board();
+      let empty = board.current().join('')
+      expect(empty).equal('')
+    });
+
+    it('should return an that contains only null values in', function() {
       console.log(Board)
       let board = new Board();
-      board.play('x', 4);
-      let expectedBoard = [null, null, null, null, 'x', null, null, null, null]
       expect(board.current()).equal(expectedBoard)
-    })
+    });
+
+    // it('should add an X to the board', () => {
+    //   console.log(Board)
+    //   let board = new Board();
+    //   board.play('x', 4);
+    //   let expectedBoard = [null, null, null, null, 'x', null, null, null, null]
+    //   expect(board.current()).equal(expectedBoard)
+    // })
 
   });
 });
