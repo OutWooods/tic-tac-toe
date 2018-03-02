41757d52a96cc37dcbf972f267b3b4f39510f461
Tagrand <thomas.a.grand@gmail.com>
2018-03-02 14:00:11 +0000

    check a board has a length of nine

--- test/boardTest.js
+++ test/boardTest.js
@@ -1,9 +1,24 @@
+const { assert } = require('chai')
+const { expect } = require('chai')
+const Board = require('../src/board').Board
 
 
 describe('Board', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+
+  describe('#current', function() {
+    it('should return an array with a length of 9 after initialising', function() {
+      console.log(Board)
+      let board = new Board();
+      expect(board.current().length).equal(9)
     });
+
+    it('should add an X to the board', () => {
+      console.log(Board)
+      let board = new Board();
+      board.play('x', 4);
+      let expectedBoard = [null, null, null, null, 'x', null, null, null, null]
+      expect(board.current()).equal(expectedBoard)
+    })
+
   });
 });
