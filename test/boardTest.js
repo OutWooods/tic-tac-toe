535b705cb159f9e6c725b861815a65b03333ba43
Tom Grand <thomas.a.grand@gmail.com>
2018-03-04 11:10:51 +0000

    add eslint mocha

--- test/boardTest.js
+++ test/boardTest.js
@@ -1,55 +1,56 @@
-const { assert } = require('chai')
-const { expect } = require('chai')
-const Board = require('../src/board').Board
+/* eslint-env mocha */
 
+const { assert } = require('chai');
+const { expect } = require('chai');
+const Board = require('../src/board').Board;
 
-describe('Board', function() {
-
-  describe('#current', function() {
-    it('should return an array with a length of 9 after initialising', function() {
-      console.log(Board)
-      let board = new Board();
-      assert.lengthOf(board.current(), (9))
+describe('Board', () => {
+  describe('#current', () => {
+    it('should return an array with a length of 9 after initialising', () => {
+      const board = new Board();
+      assert.lengthOf(board.current(), (9));
     });
 
- //unhappy with this test, want to find a better way to test it
-    it('should contain only null', function() {
-      let board = new Board();
-      let empty = board.current().join('')
-      expect(empty).equal('')
+    // unhappy with this test, want to find a better way to test it
+    it('should contain only null', () => {
+      const board = new Board();
+      const empty = board.current().join('');
+      expect(empty).equal('');
     });
-  })
+  });
+
   describe('#add', () => {
     it('should add an X to the board', () => {
-      let board = new Board();
-      expect(board.play('x', 4)[3]).equal('x')
+      const board = new Board();
+      expect(board.play('x', 4)[3]).equal('x');
     });
 
     it('should add an O to the board', () => {
-      let board = new Board();
-      expect(board.play('O', 8)[7]).equal('O')
-    })
-  })
-   describe('#isFull', () => {
-     it('starts not full', () => {
-       let board = new Board();
-       expect(board.full()).equal(false)
-     })
+      const board = new Board();
+      expect(board.play('O', 8)[7]).equal('O');
+    });
+  });
 
-     it('returns unfinished false after 8 rounds', () => {
-       let board = new Board();
-       for(var i=1; i < 9; i += 1) {
-       board.play('o', i);
-     }
-       expect(board.full()).equal(false)
-     })
+  describe('#isFull', () => {
+    it('starts not full', () => {
+      const board = new Board();
+      expect(board.full()).equal(false);
+    });
+
+    it('returns unfinished false after 8 rounds', () => {
+      const board = new Board();
+      for (let i = 1; i < 9; i += 1) {
+        board.play('o', i);
+      }
+      expect(board.full()).equal(false);
+    });
 
     it('when all spaces are filled, returns fulld', () => {
-      let board = new Board();
-      for(var i=1; i < 10; i += 1) {
-      board.play('o', i);
-    }
-      expect(board.full()).equal(true)
-    })
+      const board = new Board();
+      for (let i = 1; i < 10; i += 1) {
+        board.play('o', i);
+      }
+      expect(board.full()).equal(true);
+    });
   });
 });
